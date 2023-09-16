2e6c8c377f0d302d7d8ebc228f14d23528a5a7ae
Nimap <Nimap@DESKTOP-KK6MUO2>
2023-09-16 18:54:56 +0530

    Add project files.

---
+++ SolidPrinciple/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciple
+{
+    internal class Circle
+    {
+        public Circle(double radius) 
+        {
+            Radius = radius;
+        }
+
+        public double Radius { get; set; } 
+
+        public  double  GetArea()
+        {
+            return 3.142 * Radius * Radius;
+        }
+
+    }
+
+     public class Rectangle
+    {
+        public double length { get; set; }
+
+        public double breath { get; set; }
+
+        public double  GetArea()
+        {
+            return length * breath;
+        }
+    }
+}
---
+++ SolidPrinciple/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(5);
+            Rectangle rectangle =new Rectangle { length=4,breath=3};
+            Calcalute(c);
+            new Print().print(c);
+            Console.ReadLine();
+        }
+
+        private static void  Calcalute (Circle c)
+        {
+          //  double area = 3.142 * c.Radius * c.Radius;
+            Console.WriteLine(c.GetArea());
+        }
+
+        //private static void print(Circle c)
+        //{
+        //    Console.WriteLine(c);
+        //}
+        
+
+    }
+}
